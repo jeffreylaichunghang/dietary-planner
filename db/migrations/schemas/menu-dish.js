d4e6ef83c3d4a9aea098758ad6e7503ef56117d9
jeffreylaichunghang <laichunghangjeffrey@gmail.com>
2023-08-09 17:57:22 +0800

    added one column meal

--- db/migrations/schemas/menu-dish.js
+++ db/migrations/schemas/menu-dish.js
@@ -1,6 +1,7 @@
 module.exports = (table) => {
   table.increments()
   table.integer('dish_portion').notNullable()
+  table.string('meal')
   table.integer('menu_id').unsigned()
   table.foreign('menu_id').references('menu.id')
   table.integer('dish_id').unsigned()
