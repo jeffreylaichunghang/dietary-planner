801484f2a0b5561d969e360c380d013ea19a9ecc
jeffreylaichunghang <laichunghangjeffrey@gmail.com>
2023-08-09 16:59:13 +0800

    added columns on two join tables

--- db/migrations/schemas/menu-dish.js
+++ db/migrations/schemas/menu-dish.js
@@ -1,5 +1,6 @@
 module.exports = (table) => {
   table.increments()
+  table.integer('dish_portion').notNullable()
   table.integer('menu_id').unsigned()
   table.foreign('menu_id').references('menu.id')
   table.integer('dish_id').unsigned()
